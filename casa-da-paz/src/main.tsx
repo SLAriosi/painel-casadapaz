0066276f217de4b7f3603a9939e51ee44b8714d6
ArthurT09 <arthurtomba@gmail.com>
2024-10-01 21:13:51 -0300

    Criada a tela de cadastro e a pasta do Dashboard

--- casa-da-paz/src/main.tsx
+++ casa-da-paz/src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Login from './screens/Login.tsx'
+// import Login from './screens/Login.tsx'
+import Cadastro from './screens/Cadastro'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
+    <Cadastro />
   </StrictMode>,
 )
