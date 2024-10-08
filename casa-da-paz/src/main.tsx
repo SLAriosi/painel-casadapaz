ed1b4d589c864d55339b375d2a7c229968f384c1
ArthurT09 <arthurtomba@gmail.com>
2024-10-08 20:18:11 -0300

    tela de login

--- casa-da-paz/src/main.tsx
+++ casa-da-paz/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import Login from './screens/Login.tsx'
-import Cadastro from './screens/Cadastro'
+import Cadastro from './screens/Login'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
