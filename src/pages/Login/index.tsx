221103a75fea4a8323df0850dad07b3414692890
ArthurT09 <arthurtomba@gmail.com>
2024-10-30 22:14:07 -0300

    Instala Chackra UI

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -10,6 +10,7 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { Loading } from '../../components/Loading';
 import { Toast } from '../../components/Toast';
+import { LoginCard } from '../../components/LoginCard';
 
 
 export default function Login() {
@@ -76,79 +77,7 @@ export default function Login() {
                 color='danger'
             />
 
-            <div className={styles.main}>
-                <div className={styles.border}>
-                    <div className='d-flex flex-column align-items-center'>
-                        <h1 className='text-primary'>Login</h1>
-                        <p className='text-secundary'>
-                            Preencha os campos para logar
-                        </p>
-
-                    </div>
-                    <hr />
-
-                    <form
-                        className='needs-validation align-items-center'
-                        noValidate
-                        onSubmit={submitForm}
-                        ref={refForm}
-                    >
-                        <div
-                            className='col-md-12'
-                        >
-                            <label
-                                htmlFor='email'
-                                className='form-label'
-                            >
-                                Email
-                            </label>
-                            <input
-                                type='email'
-                                className='form-control'
-                                placeholder='Digite seu email'
-                                id='email'
-                                required
-                            />
-                            <div
-                                className='invalid-feedback'
-                            >
-                                Por favor digite seu email
-                            </div>
-                        </div>
-                        <div
-                            className='col-md-12 mt-1'
-                        >
-                            <label
-                                htmlFor='senha'
-                                className='form-label'
-                            >
-                                Senha
-                            </label>
-                            <input
-                                type='password'
-                                className='form-control'
-                                placeholder='Digite sua senha'
-                                id='senha'
-                                required
-                            />
-                            <div
-                                className='invalid-feedback'
-                            >
-                                Por favor digite sua senha
-                            </div>
-                        </div>
-                        <div className='col-md-12 mt-3'>
-                            <button
-                                className='btn btn-primary w-100'
-                                type='submit'
-                                id='botao'
-                            >
-                                Enviar
-                            </button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+            <LoginCard/>
         </>
     )
 }
